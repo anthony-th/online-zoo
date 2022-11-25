ea2be95d6b5e24f3b5235eca708c505156b8539d
Anthony <yefremov.anthony@gmail.com>
2022-11-25 23:45:30 +0600

    refactor: add 1000px main page

---
+++ pages/donate/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const progressText = document.querySelectorAll('.progress-text__block');
+const radioButton = document.querySelectorAll('.progress-bar__radio-button');
+
+function toggleInput() {
+  // progressText.classList.toggle('or3');
+  // radioButton.classList.toggle('or3');
+  progressText.forEach((block, index) => {
+    // block.classList.toggle('or3');
+    if (block.classList.contains('or3')) {
+      block.classList.remove('or3');
+    } else {
+      block.classList.add('or3');
+    }
+  })
+}
+
+// radioButton.addEventListener('click', toggleInput);
+
+radioButton.forEach(index => {
+  index.addEventListener('click', toggleInput);
+})
+
