c460392df77d393678b94c860353dc2b63d956ba
Anthony <yefremov.anthony@gmail.com>
2022-12-01 10:34:32 +0600

    fix: burger-menu open only 640px

---
+++ pages/main/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerLines = document.querySelectorAll('.burger-line');
+
+burgerMenu.addEventListener('click', () => {
+  // if (burgerMenu) {
+
+  // }
+  burgerLines.forEach((element) => {
+    console.log(element);
+    // if (arr[1].style.display == 'none ') {
+    //   console.log('hey');
+    // }
+    // arr[0].style.transform = 'rotate(45deg)';
+    // arr[0].style.position = 'relative';
+    // arr[0].style.top = '4px';
+    // arr[1].style.display = 'none';
+    // arr[2].style.transform = 'rotate(-45deg)';
+    // arr[2].style.position = 'relative';
+    // arr[2].style.bottom = '4px';
+  })
+})
