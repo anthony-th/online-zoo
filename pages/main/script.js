64e9daf4c7d3938093422c371b4dcb876248bae4
Anthony <yefremov.anthony@gmail.com>
2022-12-02 00:01:22 +0600

    feat: add shadow

--- pages/main/script.js
+++ pages/main/script.js
@@ -4,9 +4,14 @@ const burgerMenu = document.querySelector('.burger-menu');
 const burgerLines = document.querySelectorAll('.burger-line');
 const nav = document.querySelector('.nav');
 const activeColorMenu = document.querySelectorAll('.active');
+const shadow = document.querySelector('.shadow');
 
-burgerMenu.addEventListener('click', () => {
+shadow.addEventListener('click', openBurger);
+burgerMenu.addEventListener('click', openBurger);
+
+function openBurger() {
   nav.classList.toggle('burger-open');
+  shadow.classList.toggle('display-block');
   burgerMenu.classList.toggle('burger-open-now');
   activeColorMenu.forEach(e => {
     e.classList.toggle('active');
@@ -15,4 +20,4 @@ burgerMenu.addEventListener('click', () => {
   burgerLines.forEach((element) => {
     element.classList.toggle('burger-color');
   })
-})
+}
